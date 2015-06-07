9451c2c0f0dde16612cd20cb554b6a09a2086614
Mugo Petersen <mugpet@gmail.com>
2015-06-07 09:01:24 +0200

    Firebase is running!

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = angular.module("angularApp", ['ngMaterial', 'firebase']);
+
+app.controller("mainCtrl", ['$scope', '$firebaseArray', function ($scope, $firebaseArray){
+	
+	$scope.welcome ="Angular is in the house!";
+	
+	var ref = new Firebase("https://mpe-firebase-test.firebaseio.com");
+	$scope.messages = $firebaseArray(ref);
+
+
+	// https://mpe-firebase-test.firebaseio.com/
+	
+}]);
