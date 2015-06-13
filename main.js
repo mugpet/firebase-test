350bdec5d5e6f4c6374ac1b64712653feed64651
Mugo Petersen <mugpet@gmail.com>
2015-06-13 17:28:26 +0200

    Authentication works

--- main.js
+++ main.js
@@ -3,94 +3,139 @@
 var app = angular.module("angularApp", ['ngMaterial', 'firebase', 'ngRoute']);
 
 
-app.run(["$rootScope", "$location", function($rootScope, $location) {
-$rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-  // We can catch the error thrown when the $requireAuth promise is rejected
-  // and redirect the user back to the home page
-  if (error === "AUTH_REQUIRED") {
-    $location.path("views/home");
-  }
-});
+app.run(["$rootScope", "$location", function ($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function (event, next, previous, error) {
+        // We can catch the error thrown when the $requireAuth promise is rejected
+        // and redirect the user back to the home page
+        if (error === "AUTH_REQUIRED") {
+            $location.path("views/home");
+        }
+    });
 }]);
 
-app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-$routeProvider.when("/home", {
-  // the rest is the same for ui-router and ngRoute...
-  controller: "HomeCtrl",
-    controllerAs: "home",
-  templateUrl: "views/home.html",
-  resolve: {
-    // controller will not be loaded until $waitForAuth resolves
-    // Auth refers to our $firebaseAuth wrapper in the example above
-    "currentAuth": ["Auth", function(Auth) {
-      // $waitForAuth returns a promise so the resolve waits for it to complete
-      return Auth.$waitForAuth();
+app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $routeProvider.when("/home", {
+        // the rest is the same for ui-router and ngRoute...
+        controller: "HomeCtrl",
+        controllerAs: "home",
+        templateUrl: "views/home.html",
+        resolve: {
+            // controller will not be loaded until $waitForAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function (Auth) {
+                // $waitForAuth returns a promise so the resolve waits for it to complete
+                return Auth.$waitForAuth();
     }]
-  }
-}).when("/account", {
-  // the rest is the same for ui-router and ngRoute...
-  controller: "AccountCtrl",
-    controllerAs: "controller",
-  templateUrl: "views/account.html",
-  resolve: {
-    // controller will not be loaded until $requireAuth resolves
-    // Auth refers to our $firebaseAuth wrapper in the example above
-    "currentAuth": ["Auth", function(Auth) {
-      // $requireAuth returns a promise so the resolve waits for it to complete
-      // If the promise is rejected, it will throw a $stateChangeError (see above)
-      return Auth.$requireAuth();
+        }
+    }).when("/account", {
+        // the rest is the same for ui-router and ngRoute...
+        controller: "AccountCtrl",
+        controllerAs: "controller",
+        templateUrl: "views/account.html",
+        resolve: {
+            // controller will not be loaded until $requireAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function (Auth) {
+                // $requireAuth returns a promise so the resolve waits for it to complete
+                // If the promise is rejected, it will throw a $stateChangeError (see above)
+                return Auth.$requireAuth();
     }]
-  }
-}).when("/about", {
-  // the rest is the same for ui-router and ngRoute...
-  controller: "AboutCtrl",
-    controllerAs: "about",
-  templateUrl: "views/about.html",
-      resolve: {
-    // controller will not be loaded until $requireAuth resolves
-    // Auth refers to our $firebaseAuth wrapper in the example above
-    "currentAuth": ["F", function(Auth) {
-      // $requireAuth returns a promise so the resolve waits for it to complete
-      // If the promise is rejected, it will throw a $stateChangeError (see above)
-      return Auth.$requireAuth();
-    }]}
-});
-    
-    
-    
-      //  $locationProvider.html5Mode(true);
+        }
+    }).when("/about", {
+        // the rest is the same for ui-router and ngRoute...
+        controller: "AboutCtrl",
+        controllerAs: "about",
+        templateUrl: "views/about.html",
+        resolve: {
+            // controller will not be loaded until $requireAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function (Auth) {
+                // $requireAuth returns a promise so the resolve waits for it to complete
+                // If the promise is rejected, it will throw a $stateChangeError (see above)
+                return Auth.$requireAuth();
+    }]
+        }
+    });
+
+
+
+    //    $locationProvider.html5Mode(true);
 }]);
 
 
-app.controller("HomeCtrl", ["currentAuth", function(currentAuth) {
-  // currentAuth (provided by resolve) will contain the
-  // authenticated user or null if not logged in
+app.controller("HomeCtrl", ["currentAuth", function (currentAuth) {
+    // currentAuth (provided by resolve) will contain the
+    // authenticated user or null if not logged in
     console.log("Home controller active");
 }]);
-app.controller("AccountCtrl", ["currentAuth", function(currentAuth) {
-  // currentAuth (provided by resolve) will contain the
-  // authenticated user or null if not logged in
+app.controller("AccountCtrl", ["currentAuth", function (currentAuth) {
+    // currentAuth (provided by resolve) will contain the
+    // authenticated user or null if not logged in
     console.log("Account controller active");
 }]);
 
-app.controller("AboutCtrl", ["currentAuth", function(currentAuth) {
-  // currentAuth (provided by resolve) will contain the
-  // authenticated user or null if not logged in
-    console.log("Aabout controller active");
+app.controller("AboutCtrl", ["currentAuth", function (currentAuth) {
+    // currentAuth (provided by resolve) will contain the
+    // authenticated user or null if not logged in
+    console.log("About controller active");
 }]);
 
 
-app.controller("mainCtrl", ['$scope', '$firebaseArray', '$firebaseAuth',
-                            function ($scope, $firebaseArray, $firebaseAuth) {
+// let's create a re-usable factory that generates the $firebaseAuth instance
+app.factory("Auth", ["$firebaseAuth",
+  function ($firebaseAuth) {
+
+        var ref = new Firebase("https://mpe-firebase-test.firebaseio.com");
+        //      $scope.messages = $firebaseArray(ref);
+
+
+        //    var ref = new Firebase("https://docs-sandbox.firebaseio.com");
+        return $firebaseAuth(ref);
+
+
+  }
+]);
+
+app.controller("mainCtrl", ['$scope', '$firebaseArray', '$firebaseAuth', 'Auth',
+                            function ($scope, $firebaseArray, $firebaseAuth, Auth) {
 
         $scope.welcome = "Angular is in the house!";
 
-        var ref = new Firebase("https://mpe-firebase-test.firebaseio.com");
-        $scope.messages = $firebaseArray(ref);
+        $scope.auth = Auth;
 
-        $scope.authObj = $firebaseAuth(ref);
+        // any time auth status updates, add the user data to scope
+        $scope.auth.$onAuth(function (authData) {
+            $scope.authData = authData;
+        });
+
+        //   $scope.authObj = $firebaseAuth(ref);
 
         console.log("start");
+
+        $scope.authData = Auth.$getAuth();
+
+        if ($scope.authData) {
+            console.log("Logged in as:", $scope.authData.facebook.displayName);
+            console.log(JSON.stringify($scope.authData));
+        } else {
+            console.log("Logged out");
+        }
+
+
+
+        $scope.login = function () {
+
+            $scope.auth.$authWithOAuthPopup("facebook").then(function (authData) {
+                console.log("Logged in as:", authData.uid);
+            }).catch(function (error) {
+                console.log("Authentication failed:", error);
+            });
+        };
+
+        $scope.logout = function () {
+            $scope.auth.$unauth();
+        }
+
         // https://mpe-firebase-test.firebaseio.com/
 
         // Facebook authentication:
@@ -107,13 +152,8 @@ app.controller("mainCtrl", ['$scope', '$firebaseArray', '$firebaseAuth',
           console.error("Authentication failed:", error);
 });*/
 
-                                
-                                
-                                /*
-        $scope.authObj.$authWithOAuthPopup("facebook").then(function (authData) {
-            console.log("Logged in as:", authData.uid);
-        }).catch(function (error) {
-            console.log("Authentication failed:", error);
-        });*/
+
+
+
 
 }]);
